d9708f7f57109a9d9053ee35a6422ce47a8e26cb
Sheikh M. Salahuddin <smsalah@gmail.com>
2018-08-03 07:56:40 -0700

    Add files via upload

---
+++ hr_hourlywages/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hourly Wages',
+    'description': 'Pay Hourly Salary using Hourly wage field on the contact.',
+    'version': '11.0.0.0.0',
+    'website': 'https://smsalah.com/',
+    'author': 'Sheikh M. Salahuddin <smsalah@gmail.com>',
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    'data': [
+        'hr_contract_view.xml',
+		'hr_hourly_structure_view.xml'
+    ],
+    'depends': [
+        'hr_contract',
+    ],
+}
